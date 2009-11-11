28225adc2b6d6e1f56b4d9a2aee86fdbeaf716e1
Aki Saarinen <aki@akisaarinen.fi>
2009-11-11 22:19:20 +0200

    Simplify youtube regex
    No need for the first part.
    
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- modules/module_youtube.rb
+++ modules/module_youtube.rb
@@ -3,7 +3,7 @@ Kernel.load("youtube.rb")
 class Bot
   def youtube_privmsg(bot, from, reply_to, msg)
     msg.split(" ").each { |word|
-      if word =~ /.*youtube.com\/watch?/
+      if word =~ /youtube.com\/watch?/
         youtube_title = parseYoutube(word)
         bot.privmsg(reply_to, youtube_title)
       end
