dd141f9183795ac068f2f262f20d83f2af861884
Aki Saarinen <aki@akisaarinen.fi>
2009-12-15 21:46:54 +0200

    Move youtube module to single file
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/module_youtube.rb
+++ src/modules/module_youtube.rb
@@ -1,4 +1,7 @@
-Kernel.load("youtube.rb")
+require 'uri'
+require 'net/http'
+
+Kernel.load('fetch_uri.rb')
 
 class Module_Youtube
   def init_module(bot) end
@@ -9,6 +12,33 @@ class Module_Youtube
         bot.send_privmsg(reply_to, youtube_title)
       end
     }
+  end
+
+  private
+
+  def parseYoutube(url)
+    if URI.parse(url).host =~ /.*youtube\.com/
+      video = URI.parse(url).query.match(/v=([^ &]*)/)[0][2,15]
+      api = "http://gdata.youtube.com/feeds/api/videos/" + video
+      print api + "\n"
+
+      reply = fetch_uri(api)
+      return "" if (reply.code != "200") 
+      
+      title = "?"
+      rating = "?"
+      views = "?"
+
+      title = $1 if reply.body =~ /.*<title type='text'>(.*)<\/title>.*/
+      rating = $1 if reply.body =~ /.*<gd:rating average='([0-9.]*)'.*/
+      ratingcount = $1 if reply.body =~ /.*numRaters='([0-9]*)'.*/
+      views = $1 if reply.body =~ /.*<yt:statistics favoriteCount='[0-9]*' viewCount='([0-9]*)'\/>.*/
+      average = ((rating.to_f * 100).round).to_f / 100
+
+      "#{title} (rating: #{average} (#{ratingcount}), views: #{views})"
+    else
+      ""
+    end
   end
 end
 
