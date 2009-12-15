fb4107848ad640f5152261acc0c01a58636c6246
Aki Saarinen <aki@akisaarinen.fi>
2009-12-15 20:39:41 +0200

    Update Newstitle to new module format
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/module_newstitle.rb
+++ src/modules/module_newstitle.rb
@@ -1,29 +1,33 @@
 Kernel.load("fetch_uri.rb")
 
-def parseNewsTitle(url)
-  hosts = {
-    /.*iltalehti\.fi/ => /(.*) \| Iltalehti\.fi$/,
-    /.*iltasanomat\.fi/ => /(.*) - Ilta-Sanomat$/
-  }
-
-  hosts.each { |host, title_expr|
-    if URI.parse(url).host =~ host
-      reply = fetch_uri(url)
-      return "HTML error ##{reply.code}, sry :(" if (reply.code != "200") 
-      return $1 if reply.body =~ /<title>(.*)<\/title>/ && $1 =~ title_expr
-    end
-  }
-  ""
-end
-
-class Bot
-  def newstitle_privmsg(bot, from, reply_to, msg)
+class Module_Newstitle
+  def init_module(bot) end
+  
+  def privmsg(bot, from, reply_to, msg)
     msg.split(" ").each { |word|
       if word =~ /^http:\/\//
         news_title = parseNewsTitle(word)
-        bot.privmsg(reply_to, news_title) if news_title != ""
+        bot.send_privmsg(reply_to, news_title) if news_title != ""
       end
     }
+  end
+
+  private
+
+  def parseNewsTitle(url)
+    hosts = {
+      /.*iltalehti\.fi/ => /(.*) \| Iltalehti\.fi$/,
+      /.*iltasanomat\.fi/ => /(.*) - Ilta-Sanomat$/
+    }
+
+    hosts.each { |host, title_expr|
+      if URI.parse(url).host =~ host
+        reply = fetch_uri(url)
+        return "HTML error ##{reply.code}, sry :(" if (reply.code != "200") 
+        return $1 if reply.body =~ /<title>(.*)<\/title>/ && $1 =~ title_expr
+      end
+    }
+    ""
   end
 end
 
