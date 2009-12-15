8ee3e2447de8846d12ee3e883fe32ca48c5d9eab
Aki Saarinen <aki@akisaarinen.fi>
2009-12-15 20:37:30 +0200

    Convert monkey module to new format
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/module_monkey.rb
+++ src/modules/module_monkey.rb
@@ -1,24 +1,18 @@
 require "yaml"
 
-class Bot
-  def monkey_initialize(bot)
-    @monkey = Monkey.new(@base_path, @module_config["monkey_file"])
-  end
-  def monkey_privmsg(bot, from, reply_to, msg)
-    @monkey.privmsg(bot, from, reply_to, msg)
-  end
-end
-
-class Monkey
-  def initialize(base_path, filename)
-    cfg_file = base_path + "/" + filename
+class Module_Monkey
+  def init_module(bot)
+    filename = bot.module_config["monkey_file"]
+    cfg_file = bot.base_path + "/" + filename
     puts "Reading monkey config from '#{cfg_file}'"
     @expressions = YAML.load_file(cfg_file)
+    puts "Read"
   end
 
   def privmsg(bot, from, reply_to, msg)
     @expressions.value.each { |expr|
       expr.each { |trigger, reply| 
+        puts "trig"
         if m = Regexp.new(trigger).match(msg)
           updated_reply = reply.clone
           (m.length - 1).times.map { |i| i + 1 }.each { |i|
@@ -28,7 +22,7 @@ class Monkey
             end
           }
           updated_reply.gsub!("{from}", from)
-          bot.privmsg(reply_to, updated_reply)
+          bot.send_privmsg(reply_to, updated_reply)
           return
         end
       }
