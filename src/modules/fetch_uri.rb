b6978465510b36ec6e0f7e77f574cde80d35a38f
Aki Saarinen <aki@akisaarinen.fi>
2009-12-15 21:36:58 +0200

    Add post feature to fetch_uri
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/fetch_uri.rb
+++ src/modules/fetch_uri.rb
@@ -8,7 +8,20 @@ def fetch_uri(uri_str, limit = 10)
   response = Net::HTTP.get_response(URI.parse(uri_str))
   case response
   when Net::HTTPSuccess     then response
-  when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+  when Net::HTTPRedirection then fetch_uri(response['location'], limit - 1)
+  else
+    response.error!
+  end
+end
+
+def post_uri(uri_str, args = {}, limit = 10)
+  # You should choose better exception.
+  raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+  response = Net::HTTP.post_form(URI.parse(uri_str), args)
+  case response
+  when Net::HTTPSuccess     then response
+  when Net::HTTPRedirection then post_uri(response['location'], args, limit - 1)
   else
     response.error!
   end
