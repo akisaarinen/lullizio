2061072febd4954d989222bd204f6a8114117b13
Aki Saarinen <aki@akisaarinen.fi>
2009-12-15 21:44:45 +0200

    Moved URI fetching to src and added test for it
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

---
+++ test/fetch_uri_test.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'fetch_uri.rb'
+
+class TestFetchUri < Test::Unit::TestCase
+  context "Fetching URI with get" do
+    should "fetch an iltalehti uri" do
+      reply = fetch_uri("http://www.iltalehti.fi/ulkomaat/2009121510785951_ul.shtml")
+      assert_equal "200", reply.code
+      assert reply.body =~ /Iltalehti/
+    end
+  end
+end
