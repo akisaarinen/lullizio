621f6861be92852a56ce7104ddfc625295c50a12
Aki Saarinen <aki@akisaarinen.fi>
2009-12-19 17:21:29 +0200

    Added test for newstitle module
    Fetch urls from all the news providers and check whether the script
    works.
    
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

---
+++ test/module_newstitle_test.rb
@@ -0,0 +1,45 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'modules/module_newstitle.rb'
+
+class TestModule_Youtube < Test::Unit::TestCase
+  context "Newstitle module" do
+    setup do
+      @bot = mock()
+      @module = Module_Newstitle.new
+      @module.init_module(@bot)
+    end
+    
+    should "not reply to HS.fi article with title in URL" do
+      uri = "http://www.hs.fi/kotimaa/artikkeli/Kysely+Homoparien+vihkiminen+kirkossa+jakaa+kansan+mielipiteet/1135251596656"
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "reply to HS.fi article without title in URL" do
+      uri = "http://www.hs.fi/kotimaa/artikkeli/1135251596656"
+      exp_result = "Kysely: Homoparien vihkiminen kirkossa jakaa kansan mielipiteet"
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+
+    should "reply to HS.fi article from RSS without title in URL" do
+      uri = "http://www.hs.fi/kotimaa/artikkeli/1135251596656?ref=rss"
+      exp_result = "Kysely: Homoparien vihkiminen kirkossa jakaa kansan mielipiteet"
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "reply to Iltalehti.fi article" do
+      uri = "http://www.iltalehti.fi/uutiset/2009121910813607_uu.shtml"
+      exp_result = "Rekkajono Vaalimaalla pitenee | Uutiset"
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "reply to Iltasanomat.fi article" do
+      uri = "http://www.iltasanomat.fi/hyvaolo/suhteet.asp?id=1822711"
+      exp_result = "Koska flirtti puree naiseen? - Seksi ja suhteet - Hyv\344 olo"
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+  end
+end
