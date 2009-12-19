3bf1978a9e2f0dbcd5b8a7dc2452aa2861547dbf
Aki Saarinen <aki@akisaarinen.fi>
2009-12-19 17:11:29 +0200

    Add support to HS in newstitle
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/module_newstitle.rb
+++ src/modules/module_newstitle.rb
@@ -1,3 +1,5 @@
+require 'htmlentities'
+
 Kernel.load("fetch_uri.rb")
 
 class Module_Newstitle
@@ -16,18 +18,25 @@ class Module_Newstitle
 
   def parseNewsTitle(url)
     hosts = {
-      /.*iltalehti\.fi/ => /(.*) \| Iltalehti\.fi$/,
-      /.*iltasanomat\.fi/ => /(.*) - Ilta-Sanomat$/
+      [/.*iltalehti\.fi/, /.*/] => /(.*) \| Iltalehti\.fi$/,
+      [/.*iltasanomat\.fi/, /.*/] => /(.*) - Ilta-Sanomat$/,
+      [/.*hs\.fi/, /artikkeli\/[0-9]+(\?)?/] => /(.*) - HS.fi - /
     }
 
-    hosts.each { |host, title_expr|
-      if URI.parse(url).host =~ host
+    coder = HTMLEntities.new
+
+    hosts.each { |params, title_expr|
+      host = params[0]
+      path = params[1]
+      if URI.parse(url).host =~ host && URI.parse(url).path =~ path
         reply = fetch_uri(url)
-        return "HTML error ##{reply.code}, sry :(" if (reply.code != "200") 
-        return $1 if reply.body =~ /<title>(.*)<\/title>/ && $1 =~ title_expr
+        return "HTML error ##{reply.code}, sry :(" if (reply.code != "200")
+        return $1 if reply.body =~ /<title>(.*)<\/title>/ && coder.decode($1) =~ title_expr
+        return "Unable to find title"
       end
     }
     ""
+
   end
 end
 
