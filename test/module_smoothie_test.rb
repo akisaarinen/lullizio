ae82b9ca1407d5c6c155b0dafccd58db38ba2a73
Aki Saarinen <aki@akisaarinen.fi>
2010-01-24 21:18:32 +0200

    Add simple test for smoothie module
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

---
+++ test/module_smoothie_test.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'modules/module_smoothie.rb'
+
+class TestModule_Smoothie < Test::Unit::TestCase
+  context "Smoothie module" do
+    setup do
+      @bot = mock()
+      @module = Module_Smoothie.new
+      @module.init_module(@bot)
+    end
+
+    should "calculate kcals for known ingredients" do 
+      exp_result = "Laskennallinen energiasisältö: 212.1 kcal"
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "!smoothie omena:150 paaryna:150 nakki:20 tropicana:100")
+    end
+  end
+end
