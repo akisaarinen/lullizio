fd55cf5a2cf07653ad016788f6df30e408a2d6b0
Aki Saarinen <aki@akisaarinen.fi>
2010-12-27 18:18:19 +0200

    Fix input for 'say' to be utf-8 compatible in module_say
    Quite a hack to work with ruby 1.8 with least effort :)
    
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- src/modules/module_say.rb
+++ src/modules/module_say.rb
@@ -3,10 +3,27 @@ class Module_Say
   end
   
   def privmsg(bot, from, reply_to, msg)
-      system "say \"#{from} says #{msg}\""
+      system "say \"#{from} says #{convert_input(msg)}\""
   end
   
   def botmsg(bot, target, msg)
-      system "say \"I say #{msg}\""
+      system "say \"I say #{convert_input(msg)}\""
+  end
+
+private
+ 
+  def convert_input(s) 
+      p s.scan(/./m)
+      s.gsub(/:[)DdPpEe]/, " smiley ").
+       gsub(":(", " sad face ").
+       gsub("8-D", " dick ").
+       gsub("="," equals ").
+       gsub("\345","å").
+       gsub("\344","ä").
+       gsub("\366","ö").
+       gsub("\305","Å").
+       gsub("\304","Ä").
+       gsub("\326","Ö").
+       gsub(/[^a-zA-ZåäöÅÄÖ]/u,"")
   end
 end
--- src/runner.rb
+++ src/runner.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/env ruby
 
 require "bot"
 require "module_handler"
