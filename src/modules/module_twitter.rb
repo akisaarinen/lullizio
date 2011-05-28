221bc33607c344673964fb447e48a565444c9723
Aki Saarinen <aki@akisaarinen.fi>
2011-05-28 16:10:34 +0300

    Add twitter module to print out twitter status messages

---
+++ src/modules/module_twitter.rb
@@ -0,0 +1,32 @@
+require 'htmlentities'
+require 'json'
+
+Kernel.load("fetch_uri.rb")
+
+class Module_Twitter
+  def init_module(bot) end
+  
+  def privmsg(bot, from, reply_to, msg)
+    msg.split(" ").each { |word|
+      if word =~ /^http[s]?:\/\/twitter.com\/#!\/(.*)\/status\/([0-9]+)/
+        reply = requestStatus($2)
+        user = reply['user']['screen_name']
+        full = reply['user']['name']
+        text = reply['text']
+        reply_str = "@#{user} (#{full}): #{text}"
+        bot.send_privmsg(reply_to, reply_str)
+      end
+    }
+  end
+
+  def botmsg(bot,target,msg) end
+
+  private
+
+  def requestStatus(id) 
+    uri = "http://api.twitter.com/1/statuses/show/#{id}.json"
+    reply = fetch_uri(uri)
+    JSON.parse(reply.body)
+  end
+end
+
---
+++ test/module_twitter_test.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'modules/module_twitter.rb'
+
+class TestModule_Twitter < Test::Unit::TestCase
+  context "Twitter module" do
+    setup do
+      @bot = mock()
+      @module = Module_Twitter.new
+      @module.init_module(@bot)
+    end
+    
+    should "not reply to twitter homepage with HTTP" do
+      uri = "http://twitter.com"
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "not reply to twitter homepage with HTTPS" do
+      uri = "https://twitter.com"
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "reply to twitter status with HTTP" do
+      uri = "http://twitter.com/#!/beyondcontent/status/74248480662622209" 
+      exp_result = 
+        "@beyondcontent (Andrew McGarry): " + 
+        "Ryanair still using cookie history to inflate prices. My folks saved £100 by deleting cookie from yesterday's browsing session."
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+    
+    should "reply to twitter status with HTTPS" do
+      uri = "https://twitter.com/#!/beyondcontent/status/74248480662622209" 
+      exp_result = 
+        "@beyondcontent (Andrew McGarry): " + 
+        "Ryanair still using cookie history to inflate prices. My folks saved £100 by deleting cookie from yesterday's browsing session."
+      @bot.expects(:send_privmsg).with("#channel", exp_result)
+      @module.privmsg(@bot, "someone", "#channel", "some text with #{uri} inside")
+    end
+  end
+end
