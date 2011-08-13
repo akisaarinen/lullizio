23c396c7287574f37cad7d8667227cb33dccfaa5
Aki Saarinen <aki@akisaarinen.fi>
2011-08-13 14:20:59 +0300

    youtube: Cleanups to code

--- src/modules/module_youtube.rb
+++ src/modules/module_youtube.rb
@@ -30,40 +30,51 @@ class Module_Youtube
     else
       nil
     end
- end
+  end
 
   def get_formatted_title(video_id)
     api_url = "http://gdata.youtube.com/feeds/api/videos/" + video_id + "?v=2"
-    print api_url + "\n"
-
+    print "Fetching " + api_url + "\n"
     reply = fetch_uri(api_url)
-    return "" if (reply.code != "200")
+    reply.code == "200" ? format_title(reply.body) : ""
+  end
 
-    title = "?"
-    rating = "?"
-    views = "?"
+  def format_title(content)
+    title = get_title(content)
+    rating_str = get_preferred_rating_string(content)
+    views = get_views(content)
+    "#{title} (rating: #{rating_str}, views: #{views})"
+  end
 
-    title = $1 if reply.body =~ /.*<title[^>]*>(.*)<\/title>.*/
+  def get_preferred_rating_string(content)
+    likes_and_dislikes = get_likes_and_dislikes(content)
+    likes_and_dislikes ? likes_and_dislikes : get_numeric_rating(content)
+  end
 
-
-    # Prefer likes/dislikes if they exist
-    if reply.body =~ /<yt:rating numDislikes='([0-9]+)' numLikes='([0-9]+)'\/>/
-      numDislikes = $1.to_i
-      numLikes = $2.to_i
-      ratingStr = "#{numLikes} likes, #{numDislikes} dislikes"
-    # Try also numeric rating
-    elsif reply.body =~ /.*<gd:rating average='([0-9.]*)'.*/
-      avgRating = $1
-      numRaters = $1 if reply.body =~ /.*numRaters='([0-9]*)'.*/
-      valueInTwoDigits = ((rating.to_f * 100).round).to_f / 100
-      ratingStr = "#{valueInTwoDigits} (#{numRaters})"
-    # Default to unknown
+  def get_likes_and_dislikes(content)
+    if content =~ /<yt:rating numDislikes='([0-9]+)' numLikes='([0-9]+)'\/>/
+      num_dislikes = $1.to_i
+      num_likes = $2.to_i
+      "#{num_likes} likes, #{num_dislikes} dislikes"
     else
-      ratingStr = "unknown"
+      nil
     end
+  end
+  def get_numeric_rating(content)
+    if content =~ /.*<gd:rating average='([0-9.]*)'.*/
+      num_raters = $1 if reply.body =~ /.*numRaters='([0-9]*)'.*/
+      value_in_two_digits = ((rating.to_f * 100).round).to_f / 100
+      "#{value_in_two_digits} (#{num_raters})"
+    else
+      nil
+    end
+  end
 
-    views = $1 if reply.body =~ /.*<yt:statistics favoriteCount='[0-9]*' viewCount='([0-9]*)'\/>.*/
-    "#{title} (rating: #{ratingStr}, views: #{views})"
+  def get_title(content)
+    content =~ /.*<title[^>]*>(.*)<\/title>.*/ ? $1 : "?"
+  end
+  def get_views(content)
+    content =~ /.*<yt:statistics favoriteCount='[0-9]*' viewCount='([0-9]*)'\/>.*/ ? $1 : "?"
   end
 end
 
