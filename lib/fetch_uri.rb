7bedab55074777d046262bcf642a3d8225586e57
Aki Saarinen <aki@akisaarinen.fi>
2013-07-29 00:38:29 +0300

    Fix URL redirection errors
    In case the server didn't send the full URL, redirection tracker failed.
    
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- lib/fetch_uri.rb
+++ lib/fetch_uri.rb
@@ -5,10 +5,11 @@ require 'net/http'
 def fetch_uri(uri_str, limit = 10)
   raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
-  response = Net::HTTP.get_response(URI.parse(uri_str))
+  uri = URI.parse(uri_str)
+  response = Net::HTTP.get_response(uri)
   case response
   when Net::HTTPSuccess     then response
-  when Net::HTTPRedirection then fetch_uri(response['location'], limit - 1)
+  when Net::HTTPRedirection then fetch_uri(new_location(uri, response['location']), limit - 1)
   else
     response.error!
   end
@@ -17,11 +18,23 @@ end
 def post_uri(uri_str, args = {}, limit = 10)
   raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
-  response = Net::HTTP.post_form(URI.parse(uri_str), args)
+  uri = URI.parse(uri_str)
+  response = Net::HTTP.post_form(uri, args)
   case response
   when Net::HTTPSuccess     then response
-  when Net::HTTPRedirection then post_uri(response['location'], args, limit - 1)
+  when Net::HTTPRedirection then post_uri(new_location(uri, response['location']), args, limit - 1)
   else
     response.error!
   end
+end
+
+def new_location(uri, location)
+  target = URI.parse(location)
+  return target.to_s if target.host
+
+  uri.path = target.path
+  uri.opaque = target.opaque
+  uri.query = target.query
+  uri.fragment = target.fragment
+  return uri.to_s
 end
